287459fcc4bb30cd719fba032a9a9482193cbce7
Stéphanie Arlt <contact@stephaniearlt.fr>
2024-04-02 15:43:50 +0200

    Update HTML + Add Event Listeners Flèches

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+const slides = [
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+
+// Ajout Event Listeners
+const arrowLeft = document.querySelector("#banner .arrow_left");
+const arrowRight = document.querySelector("#banner .arrow_right");
+
+arrowLeft.addEventListener("click", function () {
+  console.log("Clic sur la flèche gauche");
+  // Pour changer l'image vers la gauche du carrousel
+});
+
+arrowRight.addEventListener("click", function () {
+  console.log("Clic sur la flèche droite");
+  // Pour changer l'image vers la droite du carrousel
+});
